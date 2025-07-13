ac4f21e688b0207cab593e29624c79abcebd695b
matshidisolp <mpitswane@gmail.com>
2025-07-13 15:47:42 +0700

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
       <Footer />
     </>
   );
