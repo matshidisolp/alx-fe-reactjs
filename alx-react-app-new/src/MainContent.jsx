cbb24beaf49972bfbbeab209bac363f572218dae
matshidisolp <mpitswane@gmail.com>
2025-07-18 07:53:13 +0700

    In-line CSS Styling to ReactComponents

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+  return (
+    <main
+      style={{ 
+        border: '5px solid red', 
+        padding: '10px', 
+        margin: '10px',
+        textAlign: 'center',
+        fontFamily: '"Comic Sans MS", cursive, sans-serif',
+        fontSize: '18px',
+        }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
