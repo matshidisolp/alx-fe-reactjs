ecc652bc3eade04d51a24df68b77a0c74d5a648f
matshidisolp <mpitswane@gmail.com>
2025-07-18 08:43:08 +0700

    update for counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
+      <Counter />
       <Footer />
     </>
   );
