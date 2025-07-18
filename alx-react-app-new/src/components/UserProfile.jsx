6df9f4eb71a82c513802efca6ec5b23e6f66e66a
matshidisolp <mpitswane@gmail.com>
2025-07-18 08:12:03 +0700

    soan and color blue

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,12 @@
 const UserProfile = (props) => {
    return (
      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
+       <h2>
+        <span style={{ color: 'blue' }}>{props.name}</span>
+        </h2>
+       <p>
+        Age: <span style={{ fontWeight: 'bold'}}>{props.age}}</span>
+        </p>
        <p>Bio: {props.bio}</p>
      </div>
    );
