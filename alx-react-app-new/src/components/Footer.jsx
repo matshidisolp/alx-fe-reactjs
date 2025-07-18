6dbace94621a231158a0addb3c64254d1f18d8ee
matshidisolp <mpitswane@gmail.com>
2025-07-18 08:01:27 +0700

    In-line CSS Styling to ReactComp

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer 
+    style={{ 
+        border: '1px solid gray', padding: '10px', margin: '10px', textAlign: 'center' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
