7de37a24bef4d34792d4186c6ae7c13e477f1d46
matshidisolp <mpitswane@gmail.com>
2025-07-24 07:15:58 +0700

    integrate routing for recipe details

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  return (
-    <div>
-      <h1>Recipe App with Zustand</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+         <Routes>
+            <Route path= "/" element={<>
+            <AddRecipeForm />
+            <RecipeList />
+            </>} />
+            <Route path= "/recipe/:id" element={<RecipeDetailWrapper />} />
+         </Routes>
+        </BrowserRouter>
+    );
+};
+
+//Wrapper to extract route param
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+    const { id } = useParams();
+    return <RecipeDetails recipeId={id} />;
+};
 
 export default App;
