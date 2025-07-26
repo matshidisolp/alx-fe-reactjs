1e8e30ae67c3acc1070f8893c5b87e66b2a4d4e4
matshidisolp <mpitswane@gmail.com>
2025-07-26 09:41:52 +0700

    .\FavoritesList.jsx

--- recipe-sharing-app/src/components/FavoritesList.jsx
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 import { Link } from 'react-router-dom';
 
 const FavoritesList = () => {
