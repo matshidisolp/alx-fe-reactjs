8509036d949dc490d687b42542061df2dacd8ff4
matshidisolp <mpitswane@gmail.com>
2025-07-31 07:46:28 +0700

    added search bar component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchBar from "./components/SearchBar";
 
 function App() {
     return(
         <div style={{ padding: '2rem', fontfamily: 'Arial' }}>
             <h1>GitHub User Search</h1>
             <p>Search for GitHub users by username and more.</p>
+            <SearchBar />
         </div>
     );
 }
