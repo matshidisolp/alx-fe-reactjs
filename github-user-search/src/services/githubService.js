5f2269247b9bfa0f00c5772d9ac1744e0920988a
matshidisolp <mpitswane@gmail.com>
2025-08-02 15:37:51 +0700

    extending github API integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,12 +2,24 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com";
 
+// Fetch a single user's data by username
 export const fetchUserData = async (username) => {
     try {
         const response = await axios.get(`${BASE_URL}/users/${username}`);
-            return response.data;
+        return response.data;
     } catch (error) {
-        console.error("GitHub API error:", error);
+        console.error("GitHub API error (fetchUserData):", error);
+        throw error;
+    }
+};
+
+// Advanced search in query "amy+location:london+repos:>10"
+export const searchUsers = async (query) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+        return response.data.items; // returns array of users
+    } catch (error) {
+        console.error("GitHub API error (searchUsers):", error);
         throw error;
     }
 };
