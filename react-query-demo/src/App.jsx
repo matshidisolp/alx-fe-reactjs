23a74539db4d1d4f502d322c4f69caf588c83e2b
matshidisolp <mpitswane@gmail.com>
2025-08-20 19:44:52 +0700

    .\App.css

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './components/PostsComponent';
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
+
+export default function App() {
+  // To monitor cache behavior
+  const [showPosts, setShowPosts] = useState(true);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <QueryClientProvider client={queryClient}>
+      <div style={{ maxWidth: 800, margin: '2rem auto', fontFamily: "Arial" }}>
+        <h1>React Query Demo</h1>
+        <button onClick={() => setShowPosts((s) => !s)}>
+          {showPosts ? 'Unmount Posts' : 'Mount Posts'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+        {showPosts && <PostsComponent />}
+      </div>
+    </QueryClientProvider>
+  );
+}
