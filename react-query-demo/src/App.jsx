446eb9965a4a813bafab16baa69411ce36c48289
matshidisolp <mpitswane@gmail.com>
2025-08-20 20:14:02 +0700

    App,jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,30 +1,18 @@
-import { useState } from 'react';
+import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PostsComponent from './components/PostsComponent';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false, 
-    },
-  },
-});
+const queryClient = new QueryClient();
 
-export default function App() {
-  // To monitor cache behavior
-  const [showPosts, setShowPosts] = useState(true);
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className ='App'>
+                <h1>React Query Demo</h1>
+                <PostsComponent />
+            </div>
+        </QueryClientProvider>
+    );
+} 
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div style={{ maxWidth: 800, margin: '2rem auto', fontFamily: "Arial" }}>
-        <h1>React Query Demo</h1>
-        <button onClick={() => setShowPosts((s) => !s)}>
-          {showPosts ? 'Unmount Posts' : 'Mount Posts'}
-        </button>
-
-        {showPosts && <PostsComponent />}
-      </div>
-    </QueryClientProvider>
-  );
-}
+export default App;
