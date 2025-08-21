61699bb5564dcc0a3b65b627fdda5c6eca83ebb9
matshidisolp <mpitswane@gmail.com>
2025-08-21 12:47:11 +0700

    components

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+function BlogPost() {
+    const { id } = useParams();
+
+    return (
+        <div>
+            <h2>Blog Post</h2>
+            <p>You are vieiwng blog post with ID: {id}</p>
+        </div>
+    );
+}
+
+export default BlogPost;
